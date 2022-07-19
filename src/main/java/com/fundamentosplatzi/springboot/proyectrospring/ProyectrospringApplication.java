1e4602969d219a7a6d463fd19dd5181b8d99318d
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-07-19 12:04:48 -0500

    Mi primer commit del repositorio

---
+++ src/main/java/com/fundamentosplatzi/springboot/proyectrospring/ProyectrospringApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.proyectrospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectrospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectrospringApplication.class, args);
+	}
+
+}

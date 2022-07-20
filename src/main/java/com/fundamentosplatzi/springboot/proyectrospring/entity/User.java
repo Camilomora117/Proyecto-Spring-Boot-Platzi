1f2a5dc40ce278148fb525a716622295570477bc
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-07-20 15:09:19 -0500

    Uso de JPA

---
+++ src/main/java/com/fundamentosplatzi/springboot/proyectrospring/entity/User.java
@@ -0,0 +1,90 @@
+package com.fundamentosplatzi.springboot.proyectrospring.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_user", nullable = false, unique = true)
+    private long id;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "birthday")
+    private LocalDate birthday;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonManagedReference
+    List<Post> posts = new ArrayList<>();
+
+    public User() {}
+
+    public User(String name, String email, LocalDate birthday) {
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthday=" + birthday +
+                ", posts=" + posts +
+                '}';
+    }
+}

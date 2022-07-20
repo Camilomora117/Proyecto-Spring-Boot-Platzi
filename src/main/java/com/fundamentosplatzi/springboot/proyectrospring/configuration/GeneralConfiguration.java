7ac857fd49978848ec2a03fbeb9c7205ee444f08
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-07-20 17:34:42 -0500

    Uso de anotacion value para apuntar a properties

--- src/main/java/com/fundamentosplatzi/springboot/proyectrospring/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springboot/proyectrospring/configuration/GeneralConfiguration.java
@@ -9,10 +9,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @EnableConfigurationProperties(UserPojo.class)
+@PropertySource("classpath:connection.properties")
 @Configuration
 public class GeneralConfiguration {
 
@@ -25,6 +27,18 @@ public class GeneralConfiguration {
     @Value("${value.random}")
     private String random;
 
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${username}")
+    private String userName;
+
+    @Value("${password}")
+    private String password;
+
     @Bean
     public MyBeanWithProperties function() {
         return new MyBeanWithPropertiesImplement(nombre, apellido);
@@ -33,11 +47,23 @@ public class GeneralConfiguration {
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:testdb");
-        dataSourceBuilder.username("SA");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username(userName);
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
+
+    //@Bean
+    //public DataSource dataSource() {
+    //   DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+    //   dataSourceBuilder.driverClassName("org.h2.Driver");
+    //  dataSourceBuilder.url("jdbc:h2:mem:testdb");
+    // dataSourceBuilder.username("SA");
+    //dataSourceBuilder.password("");
+    //  return dataSourceBuilder.build();
+    //}
+
+
 
 }
